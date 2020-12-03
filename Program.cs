49549f739c44b4980a745f835607a658df970c7f
Chris Marlow <cnamarlow@marquandwake.co.uk>
2020-12-03 16:30:18 +0000

    v0.1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CHStreamingAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
