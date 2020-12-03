5ef88258c0792f68d4db7e7652de1ea6b27df5e5
Chris Marlow <cnamarlow@marquandwake.co.uk>
2020-12-03 16:36:27 +0000

    v0.1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,107 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
 
 namespace CHStreamingAPI
 {
     class Program
     {
-        static void Main(string[] args)
+
+        private static readonly CancellationTokenSource canToken = new CancellationTokenSource();
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int timepoint = 0;
+            bool test = true;
+
+            //Set ability to stop through Ctrl-C in command line
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                Console.WriteLine("Cancel event triggered");
+                canToken.Cancel();
+                eventArgs.Cancel = true;
+            };
+
+            if (args.Length != 0)
+            {
+                test = int.TryParse(args[0], out timepoint);
+            }
+
+            //Capture non integer timepoint in command line arguments
+            if (test)
+            {
+
+                Console.WriteLine("Application has started. Ctrl-C to end");
+                await ReadStreamingAPI(timepoint);
+
+                //On handled errr or exit
+                Console.WriteLine("Now shutting down");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Enter numeric command line argument or leave empty for future events");
+                Console.ReadLine();
+            }
+
         }
+
+        async static Task ReadStreamingAPI(int timepoint)
+        {
+
+            using HttpClient httpClient = new HttpClient();
+
+            //Set authorization header & indefinite time out
+            httpClient.DefaultRequestHeaders.Add("Authorization", "Basic " + EncodeTo64(ConfigurationManager.AppSettings.Get("APIKey") + ":"));
+            httpClient.Timeout = TimeSpan.FromMilliseconds(Timeout.Infinite);
+
+            //If timepoint is non zero add as a query
+            string requestUri = "https://stream.companieshouse.gov.uk/companies";
+            if (timepoint != 0)
+            {
+                requestUri = requestUri + "?timepoint=" + timepoint.ToString();
+            }
+
+            try
+            {
+                //Error trapping here for 401(not authorized), 416 (timepoint out of range) etc needs response so EnsureSuccessStatusCode throws an error
+                var response = await httpClient.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead);
+                response.EnsureSuccessStatusCode();
+                using var stream = await response.Content.ReadAsStreamAsync();
+                using var reader = new StreamReader(stream);
+                while (!reader.EndOfStream && !canToken.IsCancellationRequested)
+                {
+
+                    //Read each response, ignore blanks and get timepoint from the JSON
+                    var currentLine = reader.ReadLine();
+                    if (currentLine != "")
+                    {
+                        Newtonsoft.Json.Linq.JObject json = Newtonsoft.Json.Linq.JObject.Parse(currentLine);
+                        timepoint = (int)json.SelectToken("event.timepoint");
+                    }
+                    Console.WriteLine(currentLine);
+
+                }
+
+                Console.WriteLine("Final timepoint={0}", timepoint);
+            }
+            catch (HttpRequestException exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+
+        }
+
+        static string EncodeTo64(string toEncode)
+        {
+            byte[] toEncodeAsBytes = System.Text.ASCIIEncoding.ASCII.GetBytes(toEncode);
+            string returnValue = System.Convert.ToBase64String(toEncodeAsBytes);
+            return returnValue;
+        }
+
     }
 }
---
+++ README.md
@@ -0,0 +1,24 @@
+﻿C# Console application demonstrating how to pull data from Companies House Streaming API (see https://developer-specs.company-information.service.gov.uk/streaming-api/guides/overview).
+
+Requires the addition of an App.config XML file with one setting APIKey (obtained by registering an application per the link above).
+
+<configuration>
+  <appSettings>
+    <add key="APIKey" value="" />
+  </appSettings>
+</configuration>
+
+The app is set to consume the basic company information stream.
+
+The app will accept command line argument, when present and valid it will be used as a timepoint to allow a degree of recent historic change to be pulled. On cancelling stream through CTRL+C the timepoint of the last record will be returned.
+
+See the Companies House documentation for the limitations around timepoint and issues around rate limiting & requirement for a back-off strategy (in event of repeated connection attempts).
+
+Standing on the shoulders of giants...
+
+The code for cancellation was adapted from here;
+https://stackoverflow.com/questions/48222797/capturing-cancelkeypress-to-stop-an-async-console-app-at-a-safe-point
+EncodeTo64 I found here;
+https://arcanecode.com/2007/03/21/encoding-strings-to-base64-in-c/
+The code around getting the response (so you can trap the errors) I adapted from here;
+https://stackoverflow.com/questions/30163316/httpclient-getstreamasync-and-http-status-codes
